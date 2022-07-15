9165069c1e39488bb0f60087713ad45ff703d091
Andrei Popa <andypopa@gmail.com>
2022-07-15 18:59:14 +0300

    chore: formatter service

--- projects/megakill/common/src/public-api.ts
+++ projects/megakill/common/src/public-api.ts
@@ -2,6 +2,5 @@
  * Public API Surface of common
  */
 
-export * from './lib/common.service';
-export * from './lib/common.component';
+export * from './lib/formatter.service';
 export * from './lib/common.module';
