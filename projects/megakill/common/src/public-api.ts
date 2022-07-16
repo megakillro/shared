17e0cc0041a0523d7b4447f121c44ca9b4acc5a7
Andrei Popa <andypopa@gmail.com>
2022-07-16 15:20:35 +0300

    feat: pkg

--- projects/megakill/common/src/public-api.ts
+++ projects/megakill/common/src/public-api.ts
@@ -3,6 +3,8 @@
  */
 
 export * from './lib/formatter.service';
+export * from './lib/local-files.service';
+export * from './lib/s3.service';
 export * from './lib/common.module';
 export * from './lib/ro-date-formats.constant';
 export * from './lib/injection-tokens';
